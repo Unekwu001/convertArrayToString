00768310370ee4c01096c99c87d4d0c03987cc68
Unekwu001 <tusedor360@gmail.com>
2023-05-23 23:39:13 +0100

    Add project files.

---
+++ ConsoleK/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleK
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Challenge.UpArray(new int[] {2,3,4});
+		}
+	}
+}
---
+++ ConsoleK/convertArrayTostring.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleK
+{
+	public static class Challenge
+	{
+
+		public static int[] UpArray(int[] num)
+		{
+			
+			string k = string.Join("",num);
+			int a = int.Parse(k); 
+			int b = a + 1;
+			string c = string.Join("", b);
+			int[] x = new int[c.Length];
+			for (int i = 0; i < c.Length; i++)
+			{
+				x[i] = Convert.ToInt32(c[i].ToString());
+			}
+			return x;
+
+		}
+	}
+}
